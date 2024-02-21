aeb7b0c298ac3a4a23a5f650292f894f86850c6e
floren.dev <florencia300307@gmail.com>
2024-02-21 15:29:12 +0700

    update lagii

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ContactController::class, 'index']);
+Route::post('/addcontact', [ContactController::class, 'add']);
+Route::get('/delete/{id}', [ContactController::class, 'delete']);
+Route::get('/edit/{id}', [ContactController::class, 'edit']);
+Route::post('/edit/{id}', [ContactController::class, 'update']);
